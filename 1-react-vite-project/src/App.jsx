ec4d7f41c33fd1b054206e235ac06be3b0060385
dhananjay7777 <98881567+dhananjay7777@users.noreply.github.com>
2024-01-21 02:42:57 +0530

    Second Commit

--- 1-react-vite-project/src/App.jsx
+++ 1-react-vite-project/src/App.jsx
@@ -3,7 +3,21 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 function App() {
-   return <h1>Hello World</h1>;
+   return (
+      <h1>
+         Hello World <br /> Package.json is the file which tells everything.
+         <br />
+         Run File: npm run dev in terminal. <br />
+         Change Directory: cd in terminal. <br />
+         Clear terminal: cntrl + c <br />
+         React is a library.{" "}
+         <p>
+            Library is a collection of code that can be reused by other
+            programs. It doesn't dictate overall structure and flow of program.
+            Libraries are more flexible than frameworks.
+         </p>
+      </h1>
+   );
 }
 
 export default App;
