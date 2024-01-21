2c0860cf7ceed133abf2c3adc44dab1e94ee6641
dhananjay7777 <98881567+dhananjay7777@users.noreply.github.com>
2024-01-21 21:40:05 +0530

    Second Commit

--- 2-create-new-jsx-file/new-js-or-jsx-file/src/App.jsx
+++ 2-create-new-jsx-file/new-js-or-jsx-file/src/App.jsx
@@ -1,8 +1,25 @@
 // we write all the functionality code here.
-import Coffee from "./coffee";
+
+// import function from filename
+import Coffee from "./Coffee";
 
 function App() {
-   return <Coffee />;
+   // we can export only one element
+   return (
+      // therefore if there are multiple elements, we write code inside fragment (<>)
+      <>
+         {/* function Name Coffee  */}
+         <Coffee />
+         <input type="radio" name="coff" id="dav" />
+         <label htmlFor="dav">DavidDoff</label> <br /> <br />
+         <input type="radio" name="coff" id="nes" />
+         <label htmlFor="dav">NesCafe</label> <br /> <br />
+         <input type="radio" name="coff" id="con" />
+         <label htmlFor="dav">Continental</label>
+      </>
+
+      //there are 4 elements Coffee and 3 radio buttons
+   );
 }
 
 export default App;
--- 2-create-new-jsx-file/new-js-or-jsx-file/src/coffee.jsx
+++ 2-create-new-jsx-file/new-js-or-jsx-file/src/coffee.jsx
@@ -1,5 +1,8 @@
 // we are creating a new file coffee.jsx
 
+// name of jsx file should also start with capital letter as a convention
+// the function we export is called as component
+
 function Coffee() {
    // Note: name of function should start with capital letter
    return <h2>Coffee is Ready~~~~</h2>;
