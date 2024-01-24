56be11798930ad61efc86ecf0775e31048ee13bb
dhananjay7777 <98881567+dhananjay7777@users.noreply.github.com>
2024-01-24 20:58:20 +0530

    first commit

---
+++ 6-props-plus-tailwing/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./newComponent";
+
+function App() {
+   let myObj = {
+      userName: "dukeheLLLLLLLLLLLLLLL.",
+      age: 22,
+   };
+
+   let myARR = [1, 2, 3];
+   return (
+      <>
+         <h1 className="bg-green-200 text-black p-4 rounded-xl mb-4">
+            Tailwind Test
+         </h1>
+
+         <Card
+            userName="dukeheLLLLLLLLLLL."
+            btnText="HAHAHHAHAHAHA." /*someObj={myObj} arr={myARR} this is passing value in components*/
+         />
+         <Card userName="m4rtiXXXXXXXXXXXXX" btnText="HEHHEHEHEHE." />
+      </>
+   );
+}
+
+export default App;
